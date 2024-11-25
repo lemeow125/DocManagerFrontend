52bfdafdd3b9eac5848ad0c7916c947f20cc9cdc
Keannu Bernasol <noehbernasol0@gmail.com>
2024-11-25 12:09:47 +0800

    Add role-agnostic dashboard content

---
+++ src/components/StatsWidget.tsx
@@ -0,0 +1,26 @@
+type props = {
+  title: string;
+  subtitle?: string;
+  stats: { label: string; value: number }[];
+};
+
+export default function StatsWidget(props: props) {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6 w-80 h-64">
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold">{props.title}</h2>
+        {props.subtitle && (
+          <p className="text-sm text-gray-500 mt-1">{props.subtitle}</p>
+        )}
+      </div>
+      <div className="space-y-4">
+        {props.stats.map((stat) => (
+          <div className="flex items-center justify-between">
+            <span className="font-medium">{stat.label}</span>
+            <span className="text-2xl font-bold">{stat.value}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,69 @@
+import {
+  DocumentRequestsAPI,
+  DocumentRequestType,
+  DocumentsAPI,
+} from "@/components/API";
+import { useQuery } from "@tanstack/react-query";
+import ErrorPage from "./ErrorPage";
+import LoadingPage from "./LoadingPage";
+import StatsWidget from "@/components/StatsWidget";
+
 export default function DashboardPage() {
+  const document_requests = useQuery({
+    queryKey: ["client_document_requests"],
+    queryFn: DocumentRequestsAPI,
+  });
+  const documents = useQuery({
+    queryKey: ["client_documents"],
+    queryFn: DocumentsAPI,
+  });
+  if (document_requests.isLoading || documents.isLoading) {
+    return <LoadingPage />;
+  }
+  if (document_requests.isError || documents.isError) {
+    return (
+      <ErrorPage
+        statusCode={400}
+        errorMessage="Failed to fetch dashboard. Please try again later."
+      />
+    );
+  }
   return (
     <div className="flex h-screen w-full items-center justify-center p-4">
       <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-bold text-gray-900 mb-4">
-          Dashboard Page
-        </h1>
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Dashboard</h1>
+        <div className="flex flex-row gap-2">
+          <StatsWidget
+            title={"Documents"}
+            stats={[{ label: "Available", value: documents.data.length }]}
+          />
+          <StatsWidget
+            title="Document Requests"
+            stats={[
+              {
+                label: "Pending",
+                value: documents.data.filter(
+                  (document_requests: DocumentRequestType) =>
+                    document_requests.status === "pending"
+                ).length,
+              },
+              {
+                label: "Approved",
+                value: documents.data.filter(
+                  (document_requests: DocumentRequestType) =>
+                    document_requests.status === "approved"
+                ).length,
+              },
+              {
+                label: "Denied",
+                value: documents.data.filter(
+                  (document_requests: DocumentRequestType) =>
+                    document_requests.status === "denied"
+                ).length,
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
